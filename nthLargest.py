eca695db0a2e644d869eecf1c6d740b73c78ab5c
Ishra Naznin <48322347+Ishranaznin@users.noreply.github.com>
2023-06-13 02:07:24 +0600

    Update nthLargest.py

--- nthLargest.py
+++ nthLargest.py
@@ -1,17 +1,20 @@
-def find_nth_largest(numbers, n):
+
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def hello():
+    # Take user input for the list of numbers
+    number_list = input("Enter a list of numbers, separated by spaces: ").split()
+    number_list = [int(num) for num in number_list]
+
+    # Take user input for the nth value
+    nth = int(input("Enter the value of n: "))
     numbers.sort(reverse=True)  # Sort the numbers in descending order
     if n > len(numbers):
         return "Invalid value of n"
 
-    return str(numbers[n - 1])
+    return "The {nth} largest number is: ",str(numbers[n - 1])
 
 
-# Take user input for the list of numbers
-number_list = input("Enter a list of numbers, separated by spaces: ").split()
-number_list = [int(num) for num in number_list]
-
-# Take user input for the nth value
-nth = int(input("Enter the value of n: "))
-
-result = find_nth_largest(number_list, nth)
-print(f"The {nth} largest number is: {result}")
+if __name__ == '__main__':
+      app.run(host='127.0.0.1', port=8080, debug=True)
